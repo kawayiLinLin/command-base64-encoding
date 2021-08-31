924afca18f95da7f21ce4b02e25ccbba9d966026
kawayiLinLin <1936635053@qq.com>
2021-08-31 15:47:44 +0800

    update: base64 mimeType

--- src/inputOutputHandler.js
+++ src/inputOutputHandler.js
@@ -1,9 +1,13 @@
 const { isFunction, resolvePath } = require("../utils")
 const fs = require("fs").promises
+const path = require('path')
+
+const mime = require('mime')
 
 async function inputHandler(encodingInput) {
   try {
     const result = await fs.readFile(resolvePath(encodingInput))
+    console.log(mime.getType(path.extname(encodingInput)))
     return result
   } catch {
     return encodingInput
